8de89dc8bd13a9bf1aa76d1163a2460429f68666
MaChi <loyuk@sina.com>
2016-06-17 10:59:56 +0800

    update
    新增微型渲染和跳转的控制框架

---
+++ assets/js/mileager.js
@@ -0,0 +1,26 @@
+;(function (){
+    'use strict';
+
+    var option = {
+        '1_1': {
+            url: '',
+            data: '',
+            method: 'get',
+            onBeforeLoad: function (data){
+                return data;
+            },
+            onAfterLoad: function (data){
+                return data;
+            }
+        }
+    }
+    var Mileager = function (){
+        this.version = '0.9.0';
+        this.renderString = function (){
+
+        }
+        this.pageTo = function (){
+
+        }
+    }
+})(window);
