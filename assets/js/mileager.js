d2dca7487eb8ee7e299ecf58b79a338faa70ad05
MaChi <loyuk@sina.com>
2016-06-22 18:10:23 +0800

    dev
    mileager js core dev

--- assets/js/mileager.js
+++ assets/js/mileager.js
@@ -2,7 +2,7 @@
     'use strict';
 
     var option = {
-        '1_1': {
+        '_dev': {
             url: '',
             data: '',
             method: 'get',
@@ -11,16 +11,48 @@
             },
             onAfterLoad: function (data){
                 return data;
+            },
+            onFinish: function (){
+
             }
         }
     }
-    var Mileager = function (){
+    var Mileager = function ($target){
         this.version = '0.9.0';
-        this.renderString = function (){
-
-        }
-        this.pageTo = function (){
-
-        }
+        this.option = option;
+        this.$target = $target;
     }
+
+    $.extend(Mileager.prototype, {
+        setOption: function (obj){
+            $.extend(true, this.option, obj);
+        },
+        toPage: function (id){
+            if (!this.option[id]){
+                console.error('id is not exist');
+                return;
+            }
+            var opt = this.option[id];
+            var _this = this;
+
+            var onLoaded = function (data){
+                var source = $('#' + id).html();
+                var template = Handlebars.compile(source);
+                var html = template(data);
+                
+                _this.$target.empty().append(html);
+
+                if (typeof opt.onFinish === 'function'){
+                    opt.onFinish.call(this);
+                }
+            }
+
+            //ajax here in the future;
+            setTimeout(function (){
+                onLoaded({});
+            }, 1);
+        }
+    });
+
+    window.Mileager = Mileager;
 })(window);
