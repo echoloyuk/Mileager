cd7fc5dc99ca508d61ec7488feb8a6922ea7e3e3
MaChi <loyuk@sina.com>
2016-07-08 17:23:11 +0800

    update
    update

---
+++ temp/GetAllHotCommodity.php
@@ -0,0 +1,61 @@
+<?php echo <<< EOT
+{
+    "requstresult":"1",
+    "reason":"",
+    "operation":"query",
+    "commodityItem":[
+        {
+            commodity_id:'C0001', 
+            commodity_name: '松下蒸汽电熨斗NI-W550TS',
+            company_name:'中国国航' , 
+            commodity_default_url:['http://imgeac.rocoinfo.com/upload/proimage/4/8/201511100117036284.jpg_350_350.jpg'], 
+            commodity_content_pic:['http://imgeac.rocoinfo.com/upload/editor/day_160201/2016020102574158091.jpg'], 
+            commodity_content_txt:'', 
+            commodity_mile:13040, 
+            commodity_values:0, 
+            commodity_collection:10
+        }, {
+            commodity_id:'C0002', 
+            commodity_name: '三星 Galaxy A5 (SM-A5100) 全网通4G手机 双卡双待',
+            company_name:'中国国航' , 
+            commodity_default_url:['http://ffp.scal.com.cn/FFPNewWeb/Upload/Photo/Product8042.jpeg'], 
+            commodity_content_pic:[], 
+            commodity_content_txt:'', 
+            commodity_mile:49300, 
+            commodity_values:0, 
+            commodity_collection:2
+        }, {
+            commodity_id:'C0003', 
+            commodity_name: '瑞士军刀包双肩背包 SA006',
+            company_name:'中国国航' , 
+            commodity_default_url:['http://ffp.scal.com.cn/FFPNewWeb/Upload/Photo/Product1761.jpeg'], 
+            commodity_content_pic:[], 
+            commodity_content_txt:'', 
+            commodity_mile:6000, 
+            commodity_values:50000, 
+            commodity_collection:6258
+        }, {
+            commodity_id:'C0004', 
+            commodity_name: '苹果apple watch sport（38毫米）',
+            company_name:'中国国航' , 
+            commodity_default_url:['http://ffp.scal.com.cn/FFPNewWeb/Upload/Photo/Product8742.jpeg'], 
+            commodity_content_pic:[], 
+            commodity_content_txt:'', 
+            commodity_mile:48000, 
+            commodity_values:0, 
+            commodity_collection:0
+        }, {
+            commodity_id:'C0005', 
+            commodity_name: '苹果iPod shuffle2G',
+            company_name:'中国国航' , 
+            commodity_default_url:['http://ffp.scal.com.cn/FFPNewWeb/Upload/Photo/Product8741.jpeg'], 
+            commodity_content_pic:[], 
+            commodity_content_txt:'', 
+            commodity_mile:7600, 
+            commodity_values:15, 
+            commodity_collection:0
+        }
+    ]
+}
+EOT;
+?>
